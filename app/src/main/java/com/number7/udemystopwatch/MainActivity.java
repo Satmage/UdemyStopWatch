62a47699960f0624e8d88c1d7516c6bc577cceff
Andrei Ivanou <Kleckers@yandex.ru>
2019-02-03 18:06:27 +0300

    Initial commit

---
+++ app/src/main/java/com/number7/udemystopwatch/MainActivity.java
@@ -0,0 +1,13 @@
+package com.number7.udemystopwatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
