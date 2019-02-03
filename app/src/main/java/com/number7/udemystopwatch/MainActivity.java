6314ab3a46b7ec29684345626f56e0c2c7edb632
Andrei Ivanou <Kleckers@yandex.ru>
2019-02-03 18:14:48 +0300

    update MainActivity.java - add methods for buttons
    update activity_main.xml

--- app/src/main/java/com/number7/udemystopwatch/MainActivity.java
+++ app/src/main/java/com/number7/udemystopwatch/MainActivity.java
@@ -2,6 +2,7 @@ package com.number7.udemystopwatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onMainClickStartTimer(View view) {
+    }
+
+    public void onMainClickStopTimer(View view) {
+    }
+
+    public void onMainClickResetTimer(View view) {
     }
 }
